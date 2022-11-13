7626d22af2f823642cad915c48ae9742a40e83dc
rizaozer <rizaoozer@gmail.com>
2022-11-13 23:48:42 +0200

    Kilometer to Mile converter in your calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Calculator());
+  runApp(const Calculator());
 }
 
 class Calculator extends StatelessWidget {
   const Calculator({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.orange
-      ),
-      home: MainPage(),
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const MainPage(),
     );
   }
 }
@@ -28,49 +25,71 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-
   Widget customOutlineButton(String value) {
     return Expanded(
       child: Container(
         margin: const EdgeInsets.all(7),
         child: OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          side: const BorderSide(width: 1.5),
+          style: OutlinedButton.styleFrom(
+            side: const BorderSide(width: 1.5),
+          ),
+          onPressed: () {}, // I will add functionality here
+          child: Text(
+            value,
+            style: const TextStyle(fontSize: 25),
+          ),
         ),
-        onPressed: () {}, // I will add functionality here
-      child: Text(
-        value,
-        style: const TextStyle(fontSize: 25),
       ),
-    ),),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = TextButton.styleFrom(
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+    );
     return Scaffold(
-      appBar: AppBar(title: const Text("Calculator")),
+      appBar: AppBar(
+        actions: <Widget>[
+          TextButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('CALCULATOR'),
+          ),
+          TextButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const KmToMile()),
+              );
+            },
+            child: const Text('KM TO MILE CONVERTER'),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          Expanded(child: Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.bottomRight,
-            child: const Text(
-              "0",
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.w600,
-                color: Colors.black
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.bottomRight,
+              child: const Text(
+                "0",
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black),
               ),
             ),
-          ),),
+          ),
           Row(
-          children: [
-          customOutlineButton("AC"),
-          customOutlineButton("C"),
-          customOutlineButton("%"),
-          customOutlineButton("/"),
-          ],
+            children: [
+              customOutlineButton("AC"),
+              customOutlineButton("C"),
+              customOutlineButton("%"),
+              customOutlineButton("/"),
+            ],
           ),
           Row(
             children: [
@@ -105,6 +124,45 @@ class _MainPageState extends State<MainPage> {
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class KmToMile extends StatelessWidget {
+  const KmToMile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Kilometer to Mile Converter',
+              textAlign: TextAlign.center)),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 88.0),
+            Column(
+              children: const <Widget>[
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Kilometer",
+                  ),
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    OutlinedButton(
+                        onPressed: null,
+                        child: Text("Convert"))
+                  ],
+                ),
+                Text("17,5 miles"),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
